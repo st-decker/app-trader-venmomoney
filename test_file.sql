a5ae178c682fe7c7487aaf92592ab201be43c501
st-decker <37890176+st-decker@users.noreply.github.com>
2020-01-24 18:37:00 -0600

    Breaking problem down into smaller parts to try and tackle. Tackling rounded ratings, stuck on converting prices in play store to numeric

--- test_file.sql
+++ test_file.sql
@@ -1,1 +1,36 @@
---File to play with joins
+--To do
+--Round ratings in both tables
+---App store already rounded
+--Get rid of $ sign in playstore prices
+--Convert playstore prices to numeric (like appstore)
+
+--Retreiving err with below. 
+/*
+SELECT rating
+FROM app_store_apps
+WHERE rating NOT LIKE '%.5'
+	AND rating NOT LIKE '%.0';
+*/
+
+--Corrected. LIKE/NOT LIKE is a string comparison 
+--Result: Every rating is rounded
+/*
+SELECT rating
+FROM app_store_apps
+WHERE CAST(rating AS TEXT) NOT LIKE '%.5'
+	AND CAST(rating AS TEXT) NOT LIKE '%.0'
+*/
+
+--Playstore is not rounded
+--7188 not rounded
+/*
+SELECT COUNT(rating)
+FROM play_store_apps
+WHERE CAST(rating AS TEXT) NOT LIKE '%.5'
+	AND CAST(rating AS TEXT) NOT LIKE '%.0'
+*/
+
+--How do you change the format of the price table in play_store_apps? stuck
+--Change $%.% to a number
+
+
