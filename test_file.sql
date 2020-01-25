54183cd5112d7db4bd29f53b56efda66c145eaf7
st-decker <37890176+st-decker@users.noreply.github.com>
2020-01-25 10:30:53 -0600

    Figured out rounded ratings, cleaned price

--- test_file.sql
+++ test_file.sql
@@ -1,8 +1,15 @@
---To do
+--Part 1
 --Round ratings in both tables
 ---App store already rounded
 --Get rid of $ sign in playstore prices
 --Convert playstore prices to numeric (like appstore)
+
+--Part 2
+/*
+From other file I know there are lots of duplicate apps within the play store. Copy that code over.
+Figure out how to store that table so I can use it without the duplicates
+*/
+
 
 --Retreiving err with below. 
 /*
@@ -30,7 +37,18 @@ WHERE CAST(rating AS TEXT) NOT LIKE '%.5'
 	AND CAST(rating AS TEXT) NOT LIKE '%.0'
 */
 
+--Round the ratings in the playstore
+/*
+SELECT ROUND((FLOOR(rating*2)/2), 1) AS test
+FROM play_store_apps
+WHERE rating IS NOT NULL
+ORDER BY test DESC
+*/
+
+
 --How do you change the format of the price table in play_store_apps? stuck
 --Change $%.% to a number
-
-
+/*
+SELECT CAST(REPLACE(price,'$','') AS NUMERIC) AS clean_play_price
+FROM play_store_apps
+*/
